c9382535ba5bf2d67b6d645bc1c20c023221b583
Josh Weaver <jweaver86@aim.com>
2021-02-14 15:08:52 -0500

    first commit

---
+++ JoshsJelliesAndJams/AddOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoshsJelliesAndJams
+{
+    public class AddOrder
+    {
+        private static int _orderNumberSeed = 1;
+        public int OrderNumber { get; }
+        public List<object> Items { get; set; }
+        public int CustomerNumber { get; }
+
+        public AddOrder(List<object> items, int customerNumber)
+        {
+            OrderNumber = _orderNumberSeed;
+            _orderNumberSeed++;
+            Items = items;
+            CustomerNumber = customerNumber;
+        }
+    }
+}
